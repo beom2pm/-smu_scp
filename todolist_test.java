f835f1b00bb1e4e5053c17cdde91b7c528c25e77
beom2pm <37809534+beom2pm@users.noreply.github.com>
2018-12-21 19:16:53 +0900

    Create todolist_test.java

---
+++ todolist_test.java
@@ -0,0 +1,26 @@
+package todolist.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class todolist_test {
+
+	@Test
+	void test_create() {
+		Todo_list create=new Todo_list();
+		create.List_create("test", "test description", "12/21", "12/22");
+		assertNotNull(create.todo_array[0][0]);
+		
+	}
+	@Test
+	void test_delete() {
+		Todo_list create=new Todo_list();
+		create.List_create("test", "test description", "12/21", "12/22");
+		create.List_delete("test");
+		assertNull(Todo_list.todo_array[0][0]);
+		
+		
+		
+	}
+}
