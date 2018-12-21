2a0829b21b2264f0b5696c5bf0bd6593931695b6
beom2pm <37809534+beom2pm@users.noreply.github.com>
2018-12-21 21:27:05 +0900

    Add files via upload

---
+++ PI System-h3/pi_main.java
@@ -0,0 +1,41 @@
+package pi_system;
+
+import java.util.Scanner;
+
+
+public class pi_main {
+	
+
+	public static void main(String[] args) {
+
+		
+
+		// TODO Auto-generated method stub
+		String select_num;
+
+		System.out.print("<1.Manage Contacts  2.Manage To-do list  3.Manage Appointments> Select:");
+
+		Scanner scanner =new Scanner(System.in);
+
+		select_num= scanner.next();
+
+
+
+		if(select_num.equals("1"))
+			Contacts.main_contact();
+			 
+
+		 if(select_num.equals("2")) {
+			Todo_list.main_todo();
+		}
+		
+		else if(select_num.equals("3"))
+			ManageAppointment.main_appointment();
+
+		
+
+}
+
+
+
+}
